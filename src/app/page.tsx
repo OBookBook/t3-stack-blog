38b7ae74ef10a8591af92cfa9aa152a51c2cba8c
N.I <iworksWeb@gmail.com>
2025-01-21 16:00:41 +0900

    Implement getAllBlogs API endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { clearConfigCache } from "prettier";
 
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
+  const allBlogs = await api.post.getAllBlogs();
+  console.log(allBlogs);
 
   void api.post.getLatest.prefetch();
 
@@ -39,6 +42,7 @@ export default async function Home() {
               </div>
             </Link>
           </div>
+
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
